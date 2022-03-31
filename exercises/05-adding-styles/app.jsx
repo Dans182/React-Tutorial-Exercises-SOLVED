2e5b72b8a85df0e1db8cf98e5f0e2c00164da1f7
Dans182 <dgaiteiro@gmail.com>
2022-03-31 14:07:41 +0000

    ejercicios resueltos

--- exercises/05-adding-styles/app.jsx
+++ exercises/05-adding-styles/app.jsx
@@ -4,19 +4,22 @@ import PropTypes from "prop-types";
 
 //add the styles here
 const mySuperStyles = {
-
+	background: "black",
+	fontSize: "16px",
+	border: "1px solid yellow",
 };
 
-const Badge = props => {
+const Badge = (props) => {
 	return (
 		<button style={mySuperStyles} type="button" className="btn btn-primary">
-			{props.label}<span className="badge badge-light">{props.number}</span>
+			{props.label}
+			<span className="badge badge-light">{props.number}</span>
 		</button>
 	);
 };
 Badge.propTypes = {
 	label: PropTypes.string,
-	number: PropTypes.string
+	number: PropTypes.string,
 };
 
 ReactDOM.render(<Badge label="Notifications" number="2" />, document.querySelector("#myDiv"));
